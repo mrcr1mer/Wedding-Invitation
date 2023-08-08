0c870dd1e50c5af04fefeef5ec370d63cdf138f8
Artyom Cherdantsev <22115@adata.kz>
2023-08-08 19:29:47 +0600

    fixed build

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import './assets/styles/index.scss'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import { browserDetectPlugin } from '@/plugins/browserDetect'
 import { animateDirective } from "@/directives/observer"
 
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(browserDetectPlugin)
 
 app.directive('animate', (el, binding) => {
